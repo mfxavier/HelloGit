004d4f87d37b0f245c22715147f57502211e14a7
Marlon de França Xavier <marlon.xavier@icloud.com>
2020-06-03 20:01:36 -0300

    Adicionando Projeto

---
+++ STSWithGit/src/br/edu/mmb/git/MyClassWithGit.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package br.edu.mmb.git;
+
+/**
+ * @author marlo
+ *
+ */
+public class MyClassWithGit {
+
+	/**
+	 * @param args
+	 * Integração da IDE STS com Git
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("My Application with git");
+
+	}
+
+}
